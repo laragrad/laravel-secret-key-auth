dda1d29e0918bd81903dbc1a5d1c83af3a0cc7ae
Laragrad Developer <dev@laragrad.com>
2023-04-30 16:14:15 +0300

    init commit (2)

---
+++ src/SecretKeyAuthProvider.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Laragrad\SecretKeyAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+class SecretKeyAuthProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+    
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
